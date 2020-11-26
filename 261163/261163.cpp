eaebc0a5d85c363c9738e311be96e0237ff336c9
PHATTHARANIT TESJAROEN <63010727@KMITL.AC.TH>
2020-11-27 00:53:40 +0700

    Add project files.

---
+++ 261163/261163.cpp
@@ -0,0 +1,41 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+struct input
+{
+	int num[3],sum;
+}a1, a2;
+void three() {
+	for (int x = 0; x < 3; x++) {
+		printf("input numA %d : ", x + 1);
+		scanf("%d", &a1.num[x]);
+		for(int y = 0 ; y<a1.num[x];y++){
+			printf(".");
+		}
+		printf("\n");
+		a1.sum += a1.num[x];
+	}
+	for (int x = 0; x < 3; x++) {
+		printf("input numB %d : ", x + 1);
+		scanf("%d", &a2.num[x]);
+		for (int y = 0; y < a2.num[x]; y++) {
+			printf(".");		
+		}
+		printf("\n");
+		a2.sum += a2.num[x];
+	}
+	printf("%d %d\n", a1.sum, a2.sum);
+}
+
+int main() {
+	three();
+	if (a1.sum > a2.sum) {
+		printf("Sum A win Sum B = %d", a1.sum- a2.sum);
+	}
+	if (a2.sum > a1.sum) {
+		printf("Sum B win Sum A = %d", a2.sum-a1.sum);
+	}
+	if (a2.sum == a1.sum) {
+		printf("Sum A Sum B is Draw = %d", a2.sum);
+	}
+	return 0;
+}
